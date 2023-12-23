dcc1570c6bb7c4eab35e89b9d9a99d15ca9b42a3
Aifia Mohamed <mohamedaifia34@gmail.com>
2023-12-23 17:47:06 +0100

    layout

--- src/app/components/chat-window/chat-window.component.ts
+++ src/app/components/chat-window/chat-window.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, ViewChild, inject } from '@angular/core';
 
 @Component({
   selector: 'app-chat-window',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.scss'
 })
 export class ChatWindowComponent {
-
+  emojis = [
+    "😀",
+    "😃",
+    "😄",
+    "😁",
+    "😆",
+    "😅",
+    "😂",
+    "🤣",
+    "😇",
+    "😉",
+    "😊",
+    "🙂",
+    "🙃",
+    "😋",
+    "😌",
+    "😍",
+    "🥰",
+    "😘",
+    "😗",
+    "😙",
+    "😚",
+    "🤪",
+    "😜",
+    "😝",
+    "😛",
+    "🤑",
+    "😎",
+    "🤓",
+    "🧐",
+    "🤠",
+    "🥳",
+    "🤗",
+    "🤡",
+    "😏",
+    "😶",
+    "😐",
+    "😑",
+    "😒",
+    "🙄",
+    "🤨",
+    "🤔",
+    "🤫",
+    "🤭",
+    "🤥",
+    "😳",
+    "😞",
+    "😟",
+    "😠",
+    "😡",
+    "🤬",
+    "😔",
+    "😟",
+    "😠",
+    "😡",
+    "🤬",
+    "😔",
+    "😕",
+    "🙁",
+    "😬",
+    "🥺",
+    "😣",
+    "😖",
+    "😫",
+    "😩",
+    "🥱",
+    "😤",
+    "😮",
+    "😱",
+    "😨",
+    "😰",
+    "😯",
+    "😦",
+    "😧",
+    "😢",
+    "😥",
+    "😪",
+    "🤤",
+  ];
+  showEmojiPicker = false;
+  content: String = '';
+  elementRef = inject(ElementRef);
+  toggleEmojiPicker(): void {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+  selectEmoji(emoji: string): void {
+    this.content += emoji;
+    console.log('Selected Emoji:', emoji);
+    this.showEmojiPicker = false;
+  }
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event): void {
+    if (!this.elementRef.nativeElement.contains(event.target) && this.showEmojiPicker) {
+      this.showEmojiPicker = false;
+    }
+  }
 }
